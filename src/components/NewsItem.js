6f28c6baeee4d0e716663f8c698f6db6a5435677
pritamy8062 <120549375+pritamy8062@users.noreply.github.com>
2024-06-21 04:28:22 +0530

    Add comments to NewsItem.js

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const NewsItem = (props)=> {
+        // Destructuring props to extract necessary values
         let { title, description, imageUrl, newsUrl, author, date, source } = props;
         return (
             <div className="my-3">
@@ -14,11 +15,13 @@ const NewsItem = (props)=> {
                     }> 
                         <span className="badge rounded-pill bg-danger"> {source} </span>
                     </div>
+                     {/* News image or a default image if imageUrl is not provided */}
                     <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}  </h5>
                         <p className="card-text">{description}</p>
                         <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
+                        {/* Link to read more about the news */}
                         <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
