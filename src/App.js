c57f14c3891397a5e0b9cf158edda40625f8623d
pritamy8062 <120549375+pritamy8062@users.noreply.github.com>
2024-06-21 04:31:32 +0530

    Add comments to  App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import NavBar from './components/NavBar';
 import News from './components/News';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar'
-require('dotenv').config();
+require('dotenv').config();// Importing dotenv to load environment variables
 
 const App = ()=> {
-  const pageSize = 6;
-  const apiKey = process.env.REACT_APP_NEWS_API_KEY;
-  const [progress, setProgress] = useState(0)
+  const pageSize = 6;// Getting the API key from environment variables
+  const apiKey = process.env.REACT_APP_NEWS_API_KEY;// Getting the API key from environment variables
+  const [progress, setProgress] = useState(0)// useState hook to manage the progress state
   
     return (
       <div>
@@ -24,9 +24,10 @@ const App = ()=> {
         <LoadingBar
         height={3}
         color='#f11946'
-        progress={progress} 
+        progress={progress} // Setting the progress of the loading bar
       />
         <Switch>
+          {/* Defining routes for different news categories */}
           <Route exact path="/"><News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"/></Route> 
           <Route exact path="/business"><News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business"/></Route> 
           <Route exact path="/entertainment"><News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment"/></Route> 
